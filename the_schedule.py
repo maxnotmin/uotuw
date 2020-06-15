4d99476185b75112d981c97c988f51a015c4f16b
maxnotmin <mikemax@outlook.com>
2020-06-14 19:41:53 -0700

    basic cron

--- the_schedule.py
+++ the_schedule.py
@@ -6,12 +6,31 @@ import time
 
 
 # DIGITAL OCEAN : https://www.digitalocean.com/community/tutorials/how-to-use-celery-with-rabbitmq-to-queue-tasks-on-an-ubuntu-vps
+
+
+
+
 #backend can be DataBase Connetion
 app = Celery('tasks', backend='amqp', broker='amqp://localhost//')
+
+# Another way to kickoff tasks
+"""
+app.conf.beat_schedule = {
+    # Executes every Monday morning at 7:30 a.m.
+    'add-every-monday-morning': {
+        'task': 'tasks.add',
+        'schedule': crontab(hour=7, minute=30, day_of_week=1),
+        'args': (16, 16),
+    },
+}
+"""
 
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
     # Executes every Monday morning at 7:30 a.m.
     sender.add_periodic_task(
         crontab(hour=7, minute=30, day_of_week=1),
