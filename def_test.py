1eadbae6f41b12ea1be24fee23c02de691d4daed
maxnotmin <mikemax@outlook.com>
2020-06-25 16:59:05 -0700

    added testing file

--- def_test.py
+++ def_test.py
@@ -5,25 +5,25 @@ from .media_sources import recent_video_shows, recent_podcasts
 from .read_feed import load_feed, get_vid_url, REGEX_PATTERN, BROKENSIM, loop_feed, make_dl_list
 
 
+if __name__ == "__main__":
+    # GET ALL INFO FROM BROKEN RSS
 
-# GET ALL INFO FROM BROKEN RSS
+    # Load RSS
+    bsfeed = load_feed(the_feed=BROKENSIM)
 
-#Load RSS
-bsfeed = load_feed(the_feed=BROKENSIM)
+    list_of_posts = loop_feed(theobj=bsfeed)
 
-list_of_posts = loop_feed(theobj=bsfeed)
+    # CREATE FINAL LIST OF URLS TO DOWNLOAD
+    fin_dl_list = make_dl_list(list_obj=list_of_posts)
 
-#CREATE FINAL LIST OF URLS TO DOWNLOAD
-fin_dl_list = make_dl_list(list_obj=list_of_posts)
+    # DOWNLOAD NEW VIDEOS
+    dl_vids = loop_pull_vid(thelist=fin_dl_list)
 
-# DOWNLOAD NEW VIDEOS
-dl_vids = loop_pull_vid(thelist=fin_dl_list)
+    # CLEAN OUT OLD VIDS
+    delete_played_videos(target_dir='videos')
 
-#CLEAN OUT OLD VIDS
-delete_played_videos(target_dir='videos')
+    # MOVE NEWS VIDEOS
+    move_videos(target_dir='videos')
 
-# MOVE NEWS VIDEOS
-move_videos(target_dir='videos')
-
-#END
-print("END TESTING")
+    # END
+    print("END TESTING")
