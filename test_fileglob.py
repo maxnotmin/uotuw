d6d2aaac2647e666d8e78d5786b73d944ee619ea
maxnotmin <mikemax@outlook.com>
2020-07-03 13:01:26 -0700

    test detacted

--- test_fileglob.py
+++ test_fileglob.py
@@ -1,6 +1,9 @@
 import os, sys
 
 def steam_it():
-    stream = os.popen('FileGlobLivestream videos dlive -glob "*" -shuffle -loop')
+    stream = os.popen('FileGlobLivestream videos dlive -glob "*"')
     output = stream.read()
     return True
+
+
+steam_it()
