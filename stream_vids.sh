a26a3a04b6029f4e1a386b0822d74467304ec312
maxnotmin <mikemax@outlook.com>
2020-07-03 13:58:10 -0700

    full def tests

--- stream_vids.sh
+++ stream_vids.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 # Just Run the Loop
-# FileGlobLivestream videos dlive -glob "*" -shuffle -loop
-echo pwd
-FileGlobLivestream videos dlive -glob "*"
+FileGlobLivestream videos dlive -glob "*" -shuffle -loop --yes
