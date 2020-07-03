7f10d14142b3f25e399b96303d7c5fe275f2073d
maxnotmin <mikemax@outlook.com>
2020-07-03 13:04:59 -0700

    test detacted

--- stream_vids.sh
+++ stream_vids.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Just Run the Loop
-FileGlobLivestream videos dlive -glob "*" -shuffle -loop
+# FileGlobLivestream videos dlive -glob "*" -shuffle -loop
+FileGlobLivestream videos dlive -glob "*"
